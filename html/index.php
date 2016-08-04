75e382064f7ee09fbedeb1f72cdfac2819c6dcee
shannonbit <emmanuelle.laguna@sparklabs.com.mx>
2016-08-04 11:45:50 -0500

    Se agrego primera version del maquetado

---
+++ html/index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Observatorio de Violencia de Género del Estado de Morelos y ocho municipios con Alerta de Violencia de Género (AVG) 2013-2015, folio CS-09-I-IG-056-16.</title>
+  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+  <link rel="stylesheet" href="css/custom.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+</head>
+<body>
+  <!-- Navbar -->
+  <nav class="deep-purple" role="navigation">
+    <div class="nav-wrapper container">
+
+
+    </div>
+  </nav>
+
+  <!-- Preloader -->
+  <!-- <section class="progress deep-purple lighten-4">
+  <div class="indeterminate deep-purple"></div>
+</section> -->
+
+<header class="container">
+  <div class="row valign-wrapper">
+    <a class="col s6 m2 valign" href="http://aced.org.mx" target="_blank" >
+      <img src="img/aced.svg" class=" responsive-img" alt="" />
+    </a>
+    <a class="col s6 m2 valign" href="http://indesol.gob.mx" target="_blank">
+      <img src="img/indesol.png" class="responsive-img center-align" alt="" />
+    </a>
+  </div>
+</header>
+
+<!-- PARALAX CONVENIO -->
+<section class="parallax-container">
+  <div class="parallax">
+    <img src="img/butterfly.jpg">
+  </div>
+  <div class="container">
+    <h3 class="row">
+      Observatorio de Violencia de Género del Estado de Morelos y ocho municipios con Alerta de Violencia de Género (AVG) 2013-2015, folio CS-09-I-IG-056-16.
+    </h3>
+  </div>
+</section>
+
+<!-- Map -->
+<section class="container">
+  <img src="img/mapa.svg" alt="" />
+</section>
+
+<!-- Cards Meateriales -->
+<section class="container">
+  <header>
+    <h2>Materiales</h2>
+  </header>
+  <div class="row">
+
+    <div class="col s12 m6 l4">
+
+      <div class="card sticky-action" style="overflow: hidden;">
+
+        <div class="card-image waves-effect waves-block waves-light">
+          <img class="activator" src="img/Materiales/img1.png">
+        </div>
+
+        <div class="card-content">
+
+          <span class="card-title activator grey-text text-darken-4">
+            PARTICIPACIÓN POLÍTICA DE LAS MUJERES EN MÉXICO<i class="material-icons right">more_vert</i>
+          </span>
+
+          <p>A 60 AÑOS DEL RECONOCIMIENTO DEL DERECHO AL VOTO FEMENINO</p>
+
+        </div>
+
+        <div class="card-action blue accent-2">
+          <a class="white-text" href="#">Descargar</a>
+        </div>
+
+        <div class="card-reveal" style="display: none; transform: translateY(0px);">
+
+          <span class="card-title grey-text text-darken-4">PARTICIPACIÓN POLÍTICA DE LAS MUJERES EN MÉXICO<i class="material-icons right">close</i></span>
+          <p>
+            <strong>
+              A 60 AÑOS DEL RECONOCIMIENTO DEL DERECHO AL VOTO FEMENINO
+            </strong>
+          </p>
+          <p>
+            Leela, are you alright? You got wanged on the head. I don't know what you did, Fry, but once again, you screwed up! Now all the planets are gonna start cracking wise about our mamas. Guards! Bring me the forms I need to fill out to have her taken away!
+          </p>
+
+        </div>
+
+      </div>
+    </div>
+
+
+  </div>
+</section>
+
+<!-- FOOTER -->
+<footer class="page-footer deep-purple">
+  <div class="container">
+    <div class="row">
+      <div class="col s12">
+        <p class="grey-text text-lighten-4">
+          Este material se realizó con recursos del Programa de Coinversión Social, operado por la Secretaría de Desarrollo Social, sin embargo, la ‘SEDESOL’ no necesariamente comparte los puntos de vista expresados por los autores del presente trabajo.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright deep-purple darken-2">
+    <div class="container hide-on-small-only">
+      Articulación Ciudadana para la Equidad y el Desarrollo, A. C. (ACED AC).
+    </div>
+    <div class="container hide-on-med-and-up">
+      ACED AC
+    </div>
+  </div>
+</footer>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+<script src="js/init.js"></script>
+</body>
+</html>
